603e0834a9953d95ca4ddf007f0df6da6aaf0d7d
Simon Kong <simon.kong95@gmail.com>
2019-08-17 13:24:30 -0400

    initial

---
+++ src/container/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <header>
+            <p>FREE SHIPPING on all orders over $99</p>
+        </header>
+    )
+}
+
+export default Header
